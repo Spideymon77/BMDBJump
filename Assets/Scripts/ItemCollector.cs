0698ef6b88f6d5ad802788f6e0cb2b14647963bd
ecg-idav <108599770+Spideymon77@users.noreply.github.com>
2023-02-22 20:33:47 -0700

    Added Shroomiez Powerup
    If Shroomiez are picked up, you have 16 seconds to grab items to give you 2 times the amount of points the items usually give you.
    
    Note: Figure out a way to play only 1 music file at a time.

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -8,6 +8,8 @@ public class ItemCollector : MonoBehaviour
 {
     //Text that's changing the score
     public int gems = 0;
+    public bool tripping = false;
+    public float trippingTime;
 
     //Text displaying the score
     [SerializeField] private TextMeshProUGUI gemsText;
@@ -15,6 +17,25 @@ public class ItemCollector : MonoBehaviour
     //SFX when collecting items
     [SerializeField] private AudioSource collectionSoundEffect;
     [SerializeField] private AudioSource beerSoundEffect;
+    [SerializeField] private AudioSource shroomSoundEffect;
+
+    void Update()
+    {
+        if (tripping == true)
+        {
+            trippingTime -= Time.deltaTime;
+            if (trippingTime <= 0)
+            {
+                shroomSoundEffect.Stop();
+                tripping = false;
+            }
+        }
+
+        if (tripping == false)
+        {
+            trippingTime = 16;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -27,17 +48,47 @@ public class ItemCollector : MonoBehaviour
             gemsText.text = "Score: " + gems;
         }
 
+        //Value increases if tripping
+        if (collision.gameObject.CompareTag("Gem") && tripping == true)
+        {
+            collectionSoundEffect.Play();
+            Destroy(collision.gameObject);
+            gems += 4;
+            gemsText.text = "Score: " + gems;
+        }
+
         //The Beer variant: 5 points
         if (collision.gameObject.CompareTag("Beer"))
         {
             beerSoundEffect.Play();
             Destroy(collision.gameObject);
-            gems++;
-            gems++;
-            gems++;
-            gems++;
-            gems++;
+            gems += 5;
             gemsText.text = "Score: " + gems;
         }
+
+        //Value increases if tripping
+        if (collision.gameObject.CompareTag("Beer") && tripping == true)
+        {
+            collectionSoundEffect.Play();
+            Destroy(collision.gameObject);
+            gems += 15;
+            gemsText.text = "Score: " + gems;
+        }
+
+        //The Shroomiez Variant: Powerup: items increase value * 5
+        {
+            if (collision.gameObject.CompareTag("Shroomiez"))
+            {
+                ActivatePowerUp();
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+
+    //Powerup activates setting tripping to true. After specified amount of time, powerup stops
+    void ActivatePowerUp()
+    {
+        tripping = true;
+        shroomSoundEffect.Play();
     }
 }
